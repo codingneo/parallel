290fb30f584e1b97413df7d3aff44e0f7580a18f
Hu, Yiqun <yiqun.hu@gmail.com>
2014-09-30 19:07:27 +0800

    First try of generic map function

--- parallel.go
+++ parallel.go
@@ -2,6 +2,7 @@ package parallel
 
 import (
     "fmt"
+    "reflect"
 )
 
 type Iterator struct {
@@ -10,6 +11,12 @@ type Iterator struct {
     Step int
 }
 
+//function types
+type mapf func(interface{}) interface{}
+type reducef func(interface{}, interface{}) interface{}
+type filterf func(interface{}) bool
+
+// Parallel For Loop 
 func For(iter Iterator, block func(int)) {
     factor := 4
     if iter.Step == 0 {
@@ -35,3 +42,33 @@ func For(iter Iterator, block func(int)) {
     }
 }
 
+
+func Map(in interface{}, fn mapf) reflect.Value {
+    // TODO: dynamic choose parallel factor
+    factor := 4
+
+    val := reflect.ValueOf(in)
+    ftype := reflect.ValueOf(fn).Type()
+    out := reflect.MakeSlice(
+            reflect.SliceOf(ftype.Out(0)),
+            val.Len(), val.Len())
+
+    var part = float32(val.Len()/factor)
+    
+    c := make(chan int)
+    for i := 0; i < factor; i++ {
+        go func(i int) {
+            for k := int(part)*i; k < int(part)*(i+1); k++ {
+                out.Index(k).Set(reflect.ValueOf(fn(val.Index(k).Interface())))
+            }
+            c <- i
+        }(i)
+    }
+    
+    for i := 0; i < factor; i++ {
+        k := <- c
+        fmt.Printf("%d part finished ...\n", k)
+    }
+
+    return out
+}
--- parallel_test.go
+++ parallel_test.go
@@ -1,7 +1,8 @@
 package parallel
 
 import (
-    "testing"
+	"fmt"
+	"testing"
 )
 
 func TestFor(t *testing.T) {
@@ -13,5 +14,18 @@ func TestFor(t *testing.T) {
     if data[0] != 2 {
         t.Error("Expected data[0]==2, got ", data[0])
     }
+}
+
+func TestMap(t *testing.T) {
+    data := []int{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}
+
+    result := Map(data, func(val interface{}) interface{} { return val.(int) * val.(int) })
+    out := result.Interface().([]interface{})
+    fmt.Println(out[0].(int)+out[1].(int))
+
+    if out[1].(int) != 4 {
+        t.Error("Expected data[0]==4, got ", out[1].(int))
+    }
+
 }
 
